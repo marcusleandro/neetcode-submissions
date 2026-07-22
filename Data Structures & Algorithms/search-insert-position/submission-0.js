8a514bcec21a8400493584ed0fcc6a7a408d023e
Marcus Leandro Rosa Santos <marcus.leandro.rosa@gmail.com>
2026-07-21 21:15:19 -0300

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    searchInsert(nums, target) {
+        let left = 0;
+        let right = nums.length - 1;
+
+        while(left <= right) {
+            const mid = Math.floor(left + (right - left) / 2);
+
+            if(nums[mid] === target) {
+                return mid;
+            }
+            else if(nums[mid] > target) {
+                right = mid - 1;
+            }
+            else {
+                left = left + 1;
+            }
+        }
+
+        return left;
+    }
+}
